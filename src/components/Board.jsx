d852c482e343a94310d505f929408edab1d3ec87
Endijs Dārznieks <endijs.darznieks@accenture.com>
2023-01-25 17:21:00 +0200

    Finished making the game

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,11 +1,27 @@
+import { useState } from 'react';
 import Square from './Square';
 
 const Board = () => {
-  const renderSquare = (i) => {
-    return <Square value={i} />;
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const handleClick = (i) => {
+    const squaresTemp = squares.slice();
+    if (calculateWinner(squaresTemp) || squares[i]) return;
+    squaresTemp[i] = xIsNext ? 'X' : 'O';
+    setSquares(squaresTemp);
+    setXIsNext(!xIsNext);
   };
 
-  const status = 'Next player: X';
+  const renderSquare = (i) => {
+    return <Square value={squares[i]} onClick={() => handleClick(i)} />;
+  };
+
+  const winner = calculateWinner(squares);
+  let status;
+
+  if (winner) status = 'Winner: ' + winner;
+  else status = 'Next player: ' + (xIsNext ? 'X' : 'O');
 
   return (
     <div>
@@ -27,6 +43,27 @@ const Board = () => {
       </div>
     </div>
   );
+};
+
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] == squares[b] && squares[a] == squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 };
 
 export default Board;
