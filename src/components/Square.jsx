33346f5c0d18dc5f81e2e62e22eb89147ee6dcc2
Endijs Dārznieks <endijs.darznieks@accenture.com>
2023-01-25 15:40:00 +0200

    Restructured code

---
+++ src/components/Square.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+const Square = () => {
+  const [value, setValue] = useState(null);
+
+  return (
+    <button className="square" onClick={() => setValue('X')}>
+      {value}
+    </button>
+  );
+};
+
+export default Square;
